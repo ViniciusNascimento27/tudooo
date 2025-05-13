c64eceac2de4fe53ef1ea4157e4ffda29fa8d107
VINICIUS SCHÜRHAUS DO NASCIMENTO <vinicius_nascimen5@estudante.sesisenai.org.br>
2025-05-13 16:41:05 -0300

    Pegando o ID do Usuario

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const fs = require("fs")
 const crypto = require("crypto")
 let uuid = crypto.randomUUID()
+const express = require("express")
+const app = express()
 
 
 
@@ -34,6 +36,21 @@ const salvarLogs = (name) => {
     })
  })
 
+
+
+ app.get(`/:id`, (req, res) => {
+    try{
+        const id = req.params.id
+        const dados = fs.readFileSync("logs.txt", `${id}`)
+        const dadosID = dados.find(dado => dado.startsWith(`${id} -`))
+        res.status(200).json({
+            msg : `Dados lidos com sucesso para o ID: ${id}`,
+            dados: dadosID
+        })
+    }catch(erro){
+        res.status(500).json({erro: "Erro ao ler os logs"})
+    }
+ })
  app.listen(3000, () => {
     console.log("Servidor rodando porta 3000")
  })
